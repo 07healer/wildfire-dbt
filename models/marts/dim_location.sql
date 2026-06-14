e9a32a31d79688efde30882e1a2db6f90e4e640e
Vinit Sharma <svinit0907@gmail.com>
2026-06-14 22:11:11 +0200

    updated

--- models/marts/dim_location.sql
+++ models/marts/dim_location.sql
@@ -2,14 +2,12 @@ select distinct
     {{ dbt_utils.generate_surrogate_key(['latitude', 'longitude']) }} as location_id,
     latitude,
     longitude,
-    state,
-    county
+    state
 from {{ ref('stg_historical_fires') }}
 union distinct
 select distinct
     {{ dbt_utils.generate_surrogate_key(['latitude', 'longitude']) }} as location_id,
     latitude,
     longitude,
-    cast(null as string) as state,
-    cast(null as string) as county
+    cast(null as string) as state
 from {{ ref('stg_fire_streaming') }}
