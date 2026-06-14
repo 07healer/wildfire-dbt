a9f16def8d84021174e002706ac1a3ef1d48f21a
Vinit Sharma <svinit0907@gmail.com>
2026-06-14 22:03:07 +0200

    changes

--- models/staging/stg_historical_fires.sql
+++ models/staging/stg_historical_fires.sql
@@ -1,16 +1,12 @@
 select
     FOD_ID                  as fire_id,
-    FIRE_NAME               as fire_name,
     FIRE_YEAR               as fire_year,
-    DISCOVERY_DATE          as discovery_date_julian,
-    DISCOVERY_DOY           as discovery_day_of_year,
+    DISCOVERY_DATE_CONVERTED as discovery_date,
     STAT_CAUSE_DESCR        as cause,
     FIRE_SIZE               as fire_size_acres,
     FIRE_SIZE_CLASS         as fire_size_class,
     LATITUDE                as latitude,
     LONGITUDE               as longitude,
     STATE                   as state,
-    COUNTY                  as county,
-    OWNER_DESCR             as owner,
     'historical'            as source
 from {{ source('wildfire_data', 'historical_fires') }}
